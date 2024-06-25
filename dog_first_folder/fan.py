371a9dbc7499ead047e7dc353ff18e7fd4f158b9
gyanpriya <77888627+gyanpriya@users.noreply.github.com>
2024-06-25 16:57:49 +0530

    Update fan.py

--- dog_first_folder/fan.py
+++ dog_first_folder/fan.py
@@ -1,4 +1,4 @@
 print("This is first python file in dog folder")
 print("____")
-print("This is to learn merge and conflict from edit from github from first and second branch to main branch.")
+print("This is to learn merge and conflict from edit from github from second branch to main branch.")
 
